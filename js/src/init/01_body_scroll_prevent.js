2e4129c24ad7848b45fc0b7ecb44e62c6c366023
Kosuke Naito(Pocket.Inc) <kosukenaito13@gmail.com>
2023-11-01 16:23:30 +0900

    add / ヘッダー、フッター(PC)

---
+++ js/src/init/01_body_scroll_prevent.js
@@ -0,0 +1,53 @@
+/*------------------------------------
+モーダル表示時の背景固定処理
+bodyScrollPrevent(true); //スクロール禁止
+bodyScrollPrevent(false, modalArea); //スクロール解除
+------------------------------------*/
+
+// transitionendイベントを一回だけ呼び出すための関数
+const addEventListenerOnce = (node, event, callback) => {
+  const handler = (e) => {
+    callback.call(node, e);
+    node.removeEventListener(event, handler);
+  };
+  node.addEventListener(event, handler);
+};
+
+// モーダル表示時の背景固定処理
+const bodyScrollPrevent = (flag, modal) => {
+  let scrollPosition;
+
+  // スクロールバーの幅を取得
+  setScrollbarWidth();
+
+  if (flag) {
+    body.style.paddingRight = `${scrollBarWidth}px`;
+    header.style.paddingRight = `${scrollBarWidth}px`;
+
+    if (isiOS) {
+      scrollPosition = -window.pageYOffset;
+      Object.assign(body.style, {
+        position: "fixed",
+        width: "100%",
+        top: `${scrollPosition}px`,
+      });
+    } else {
+      body.style.overflow = "hidden";
+    }
+  } else {
+    body.style.paddingRight = "";
+    header.style.paddingRight = "";
+
+    if (isiOS) {
+      scrollPosition = parseInt(body.style.top.replace(/[^0-9]/g, ""), 10);
+      Object.assign(body.style, {
+        position: "",
+        width: "",
+        top: "",
+      });
+      window.scrollTo(0, scrollPosition);
+    } else {
+      body.style.overflow = "";
+    }
+  }
+};
---
+++ js/src/script/02_check_webp_support.js
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------
+Pocket.Inc WebP対応チェッカ - 2023-10-31
+----------------------------------------------------------*/
+async function supportsWebP() {
+  return new Promise((resolve) => {
+    const webp = new Image();
+    webp.onload = () => {
+      // 画像のロードが成功すればWebPがサポートされていると判断
+      resolve(true);
+    };
+    webp.onerror = () => {
+      // 画像のロードが失敗すればWebPがサポートされていないと判断
+      resolve(false);
+    };
+    // 実際のWebPファイルのパスを指定
+    webp.src = "img/common/header_logo.webp";
+  });
+}
+
+(async () => {
+  if (await supportsWebP()) {
+    document.body.classList.add("webp-support");
+  } else {
+    document.body.classList.add("no-webp-support");
+  }
+})();
