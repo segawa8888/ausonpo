bc4712db7000866ba4ed843019e5fc43d8ea136b
内藤 皓介(Pocket Inc.) <naito@pocket-kobe.co.jp>
2023-11-01 23:18:11 +0900

    add / SPフッターメニュー実装

--- js/src/script/03_toggle_menu.js
+++ js/src/script/03_toggle_menu.js
@@ -2,12 +2,21 @@
 Pocket.Inc トグルメニュー - 2023-11-2
 ----------------------------------------------------------*/
 document.addEventListener("DOMContentLoaded", () => {
-  // ボタンのクリックイベントのリスナーを設定
   document.querySelectorAll(".js-drawer-toggle-btn").forEach((button) => {
     button.addEventListener("click", function () {
       // このボタンの隣接要素にslideToggleを適用
       const adjacentElement = this.nextElementSibling;
       if (adjacentElement && adjacentElement.classList.contains("js-drawer-toggle-content")) {
+        slideToggle(adjacentElement, this);
+      }
+    });
+  });
+
+  document.querySelectorAll(".js-footer-toggle-btn").forEach((button) => {
+    button.addEventListener("click", function () {
+      // このボタンの隣接要素にslideToggleを適用
+      const adjacentElement = this.nextElementSibling;
+      if (adjacentElement && adjacentElement.classList.contains("js-footer-toggle-content") && ww <= 520) {
         slideToggle(adjacentElement, this);
       }
     });
