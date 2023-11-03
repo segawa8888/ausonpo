9f01a00ae3d9620b4b0ff6d0a1d7751e2faeb9b4
Kosuke Naito(Pocket.Inc) <kosukenaito13@gmail.com>
2023-11-03 17:10:30 +0900

    fix / header nav / SP / スクロールアイコン追加

--- js/script.js
+++ js/script.js
@@ -226,3 +226,22 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+/*----------------------------------------------------------
+Pocket.Inc グローバルナビのスクロールアイコンのフェードアウト処理 - 2023-11-2
+----------------------------------------------------------*/
+document.addEventListener("DOMContentLoaded", () => {
+  // 対象となる要素を取得
+  const headerNav = document.querySelector(".l-header-nav");
+  // 初回スクロールのフラグ
+  let isFirstScroll = true;
+  // スクロールイベントのリスナーを追加
+  headerNav.addEventListener("scroll", function () {
+    // 初回スクロール時にのみクラスを追加する
+    if (isFirstScroll) {
+      this.classList.add("is-scrolled");
+      // 初回のスクロールを検知した後はフラグをfalseにしてイベントが再発火しないようにする
+      isFirstScroll = false;
+    }
+  });
+});
