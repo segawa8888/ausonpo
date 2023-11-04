eb915180971cdafaf206fceb57404a292bd59d5f
Kosuke Naito(Pocket.Inc) <kosukenaito13@gmail.com>
2023-11-04 16:59:58 +0900

    update / ベースコーディング(進行中)

--- js/temp/script.js
+++ js/temp/script.js
@@ -76,13 +76,14 @@ Pocket.Inc グローバルナビのスクロールアイコンのフェードア
 document.addEventListener("DOMContentLoaded", () => {
   // 対象となる要素を取得
   const headerNav = document.querySelector(".l-header-nav");
+  const headerNavScroll = document.querySelector(".l-header-nav__scroll");
   // 初回スクロールのフラグ
   let isFirstScroll = true;
   // スクロールイベントのリスナーを追加
   headerNav.addEventListener("scroll", function () {
     // 初回スクロール時にのみクラスを追加する
     if (isFirstScroll) {
-      this.classList.add("is-scrolled");
+      headerNavScroll.classList.add("is-scrolled");
       // 初回のスクロールを検知した後はフラグをfalseにしてイベントが再発火しないようにする
       isFirstScroll = false;
     }
