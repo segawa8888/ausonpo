72969721563b6a1b3b1417678563c7804ae97db0
内藤 皓介(Pocket Inc.) <naito@pocket-kobe.co.jp>
2023-11-07 19:58:58 +0900

    update / おすすめ記事 スライダー実装

--- assets/js/script.js
+++ assets/js/script.js
@@ -300,13 +300,27 @@ Pocket.Inc グローバルナビのスクロールアイコンのフェードア
 ----------------------------------------------------------*/
 //コンポーネント読み込み後
 document.addEventListener("componentsLoaded", () => {
-  // 対象となる要素を取得
-  const pickupSlider = document.querySelector(".js-pickup-slider");
+  const pickupSlider = document.querySelector(".js-pickup-slider"); // 対象となる要素を取得
   if (pickupSlider) {
     new Splide(pickupSlider, {
       type: "loop",
       perPage: 1,
       arrows: false,
+    }).mount();
+  }
+  const postCardSlider = document.querySelector(".js-post-card-slider"); // 対象となる要素を取得
+  if (postCardSlider) {
+    new Splide(postCardSlider, {
+      perPage: 3,
+      arrows: false,
+      gap: "0.5rem",
+      breakpoints: {
+        520: {
+          type: "loop",
+          perPage: 1,
+          padding: "2.5rem",
+        },
+      },
     }).mount();
   }
 });
