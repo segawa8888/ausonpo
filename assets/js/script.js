04f1628139686a1870a23b8f5e1f4d3c0b9be69a
Yoshiaki Numajiri <afmaar128@gmail.com>
2023-11-16 18:29:28 +0900

    fix pet projects No1 mv slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -374,6 +374,68 @@ document.addEventListener("componentsLoaded", () => {
       },
     }).mount();
   }
+
+    //ペットの保険 mvスライダー
+    const mvSlider = document.querySelector(".js-mv-slider"); // 対象となる要素を取得
+    if (mvSlider) {
+      new Splide(mvSlider, {
+        autoplay: true,
+        type   : 'loop',
+        perMove: 1,
+        arrows: false,
+        gap: '-1.8rem',
+        focus: 0,
+        padding: '9.625rem',
+        updateOnMove: true,
+        autoScroll: {
+          speed: 0.3,
+          pauseOnHover: false,
+        },
+        classes: {
+          pagination: "splide__pagination p-pet-column-pagination",
+          page: "splide__pagination__page p-pet-column-page",
+        },
+        breakpoints: {
+          520:{
+          perPage: 1,
+          gap: '0.78rem',
+          padding: '2.099rem',
+          }
+        }
+      }).mount();
+    }
+  
+    //ワンちゃんとの暮らしに役立つコラムのスライダー
+    const petSlider = document.querySelector(".js-column-slider"); // 対象となる要素を取得
+    if (petSlider) {
+      new Splide(petSlider, {
+        autoplay: true,
+        type   : 'loop',
+        perPage: 3,
+        perMove: 1,
+        arrows: false,
+        gap: '3.125rem',
+        focus: 0,
+        padding: '3.125rem',
+        autoScroll: {
+          speed: 0.5,
+          pauseOnHover: false,
+        },
+        classes: {
+          pagination: "splide__pagination p-pet-column-pagination",
+          page: "splide__pagination__page p-pet-column-page",
+        },
+        breakpoints: {
+          520:{
+          type: "loop",
+          perPage: 1,
+          gap: '.625rem',
+          padding: '1.875rem',
+          }
+        }
+      }).mount();
+    }
+  
 });
 
 /*----------------------------------------------------------
@@ -455,73 +517,6 @@ document.addEventListener("componentsLoaded", () => {
       document.getElementById(id).classList.add("is-active");
     });
   });
-});
-
-/*----------------------------------------------------------
-Yoshiaki Numajiri ペットの保険 スライダー - 2023-11-14
-----------------------------------------------------------*/
-//コンポーネント読み込み後
-document.addEventListener("componentsLoaded", () => {
-  //mvスライダー
-  const mvSlider = document.querySelector(".js-mv-slider"); // 対象となる要素を取得
-  if (mvSlider) {
-    var splide = new Splide(mvSlider, {
-      autoplay: true,
-      type: "loop",
-      perMove: 1,
-      arrows: false,
-      gap: "-1.8rem",
-      focus: 0,
-      padding: "9.625rem",
-      updateOnMove: true,
-      autoScroll: {
-        speed: 0.3,
-        pauseOnHover: false,
-      },
-      classes: {
-        pagination: "splide__pagination p-pet-column-pagination",
-        page: "splide__pagination__page p-pet-column-page",
-      },
-      breakpoints: {
-        520: {
-          perPage: 1,
-          gap: "0.78rem",
-          padding: "2.099rem",
-        },
-      },
-    }).mount();
-  }
-
-  //ワンちゃんとの暮らしに役立つコラムのスライダー
-  const petSlider = document.querySelector(".js-column-slider"); // 対象となる要素を取得
-  if (petSlider) {
-    var splide = new Splide(petSlider, {
-      autoplay: true,
-      type: "loop",
-      perPage: 3,
-      perMove: 1,
-      arrows: false,
-      gap: "3.125rem",
-      focus: 0,
-      padding: "3.125rem",
-      autoScroll: {
-        speed: 0.5,
-        pauseOnHover: false,
-      },
-      classes: {
-        pagination: "splide__pagination p-pet-column-pagination",
-        page: "splide__pagination__page p-pet-column-page",
-      },
-      breakpoints: {
-        520: {
-          type: "loop",
-          perPage: 1,
-          gap: ".625rem",
-          padding: "1.875rem",
-        },
-      },
-    }).mount();
-  }
 });
 
 /*----------------------------------------------------------
