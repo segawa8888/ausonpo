b5a3fa9d7006995fab57962a4e30a96de1ef11d5
Kai Shionoya <kai.shionoya@KainoMacBook-Air.local>
2023-11-17 14:56:45 +0900

    add: ポップアップウィンドウのスクリプトを現行サイトよりコピー

--- src/js/temp/script.js
+++ src/js/temp/script.js
@@ -342,6 +342,19 @@ document.addEventListener("componentsLoaded", () => {
   }
 });
 
+var win2
+function openwin2(pop,winname){
+	var avW = 700;
+	var avH = 650;
+	win2 = open(pop,'popup',"toolbar=0,location=0,directories=0,status=0,menubar=0,scrollbars=1,resizable=1,top=0,left=0,width=" + avW + ",height=" + avH + "");
+	win2.opener = self;
+	win2.focus();
+}
+// 親ウインドウが閉じる際に子ウインドウも閉じる
+onunload = function() {
+	if(!win2) return false;
+	win2.close();
+}
 /*----------------------------------------------------------
 Pocket.Inc コンポーネントの読み込み管理 - 2023-11-4
 ・全てのコードの最後に読み込む
