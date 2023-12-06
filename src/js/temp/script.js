20c2768ffea873ff2595b5c82c966505d7cff969
tomoya <n045ca3142@gmail.com>
2023-12-06 22:24:43 +0900

    add: ペット・画像　他

--- src/js/temp/script.js
+++ src/js/temp/script.js
@@ -1276,6 +1276,28 @@ function calculate(){
     ];
 
 
+/*----------------------------------------------------------
+inoue トグルボタン（全て見る）機能 - 2023-12-04
+----------------------------------------------------------*/
+document.addEventListener("componentsLoaded", () => {
+    document.getElementById('view-all').addEventListener('click', function () {
+        // hidden-link クラスを持つすべての要素を取得
+        const hiddenLinks = document.querySelectorAll('.hidden-link');
+
+        // 表示状態が変更されているかどうかを確認
+        const isHidden = hiddenLinks[0].style.display === 'none' || hiddenLinks[0].style.display === '';
+
+        // hidden-link クラスを持つ要素の表示状態を切り替え
+        hiddenLinks.forEach(link => {
+            link.style.display = isHidden ? 'inline-block' : 'none';
+        });
+
+        // ボタンのテキストを変更
+        this.querySelector('a').textContent = isHidden ? '電子広告を少なく表示する' : '電子広告を全て見る';
+    });
+
+});
+
 //コンポーネント読み込み後
 document.addEventListener("componentsLoaded", () => {
   const tabContainer = document.querySelector(".c-tab");
@@ -1382,28 +1404,6 @@ document.addEventListener("componentsLoaded", () => {
       },
     },
   }).mount();
-});
-
-/*----------------------------------------------------------
-inoue トグルボタン（全て見る）機能 - 2023-12-04
-----------------------------------------------------------*/
-document.addEventListener("componentsLoaded", () => {
-    document.getElementById('view-all').addEventListener('click', function () {
-        // hidden-link クラスを持つすべての要素を取得
-        const hiddenLinks = document.querySelectorAll('.hidden-link');
-
-        // 表示状態が変更されているかどうかを確認
-        const isHidden = hiddenLinks[0].style.display === 'none' || hiddenLinks[0].style.display === '';
-
-        // hidden-link クラスを持つ要素の表示状態を切り替え
-        hiddenLinks.forEach(link => {
-            link.style.display = isHidden ? 'inline-block' : 'none';
-        });
-
-        // ボタンのテキストを変更
-        this.querySelector('a').textContent = isHidden ? '電子広告を少なく表示する' : '電子広告を全て見る';
-    });
-
 });
 
 /*----------------------------------------------------------
