ea1c1034a2bb053f365c329abbdda4bbb613a563
Kosuke Naito(Pocket.Inc) <kosukenaito13@gmail.com>
2023-12-08 11:53:58 +0900

    add / ヘッダーメニュー hover処理追加、ドロワメニューのtabでの開閉処理の追加

--- src/js/init/01_init.js
+++ src/js/init/01_init.js
@@ -9,45 +9,19 @@ const isiOS = ["iphone", "ipad", "macintosh"].some((device) => ua.includes(devic
 const isMobile = ["iphone", "ipad", "macintosh", "android"].some((device) => ua.includes(device) && "ontouchend" in document); //Mobile判定
 const currentUrl = window.location.href; //URLの取得
 const currentPath = window.location.pathname; //パス
-const resizeTimerCmn = null; //resizeイベント用タイマー
-let ww; //window width
-let wh; //window height
 
 const html = document.querySelector("html"); //html要素
 const body = document.querySelector("body"); //body要素
+let main; //main要素
+let header; //header要素
 let scrollBarWidth = window.innerWidth - body.clientWidth; //スクロールバーの幅
-
-//1.スクロールバーを除いたブラウザサイズの取得
-const setWindowSize = () => {
-  ww = document.documentElement.clientWidth;
-  wh = document.documentElement.clientHeight;
-};
-setWindowSize();
 
 //2.スクロールバーのサイズの取得
 const setScrollbarWidth = () => {
   scrollBarWidth = window.innerWidth - body.clientWidth;
 };
 
-//3.loadイベント(読み込み完了時の処理)
-window.addEventListener("load", setWindowSize);
-
 document.addEventListener("componentsLoaded", () => {
-  const header = document.querySelector(".js-header"); //header要素
-  const main = document.querySelector(".js-main"); //main要素
-  //4.resizeイベント
-  //resizeイベントが発生しないようにする処理(回転時のみ)
-  //200ミリ秒ごとにイベントを走らせ重複を避ける
-  const resizeHandlerCmn = () => {
-    if (resizeTimerCmn) {
-      clearTimeout(resizeTimerCmn);
-    }
-    resizeTimerCmn = setTimeout(setWindowSize, 200);
-  };
-
-  if (isMobile) {
-    window.addEventListener("orientationchange", resizeHandlerCmn);
-  } else {
-    window.addEventListener("resize", resizeHandlerCmn);
-  }
+  main = document.querySelector(".js-main"); //main要素
+  header = document.querySelector(".js-header"); //header要素
 });
