f8123e52b4b68bbdb204218a6669add4f1a2172d
Kosuke Naito(Pocket.Inc) <kosukenaito13@gmail.com>
2023-12-11 15:37:23 +0900

    fix / お客様の声 / 正規画像差し替え、PCでの2要素以下の際の中央寄せ、その他コード調整

--- src/js/temp/script.js
+++ src/js/temp/script.js
@@ -317,6 +317,8 @@ document.addEventListener("componentsLoaded", () => {
       perPage: 3,
       arrows: false,
       pagination: false,
+      //focus: "center",
+      gap: "2.5rem",
       breakpoints: {
         520: {
           perPage: 1,
@@ -338,7 +340,6 @@ document.addEventListener("componentsLoaded", () => {
       },
     }).mount();
   }
-
 
   //猫の保険特長のスライダー
   const catFeatureSlider = document.querySelector(".js-cat-feature-slider"); // 対象となる要素を取得
