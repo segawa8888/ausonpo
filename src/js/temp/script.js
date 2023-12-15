e14188ad777ea220cdb6d39e0331c342b1758cfd
内藤 皓介(Pocket Inc.) <naito@pocket-kobe.co.jp>
2023-12-15 16:08:35 +0900

    update / bycle系下層ページでのナビゲーションカレント表示

--- src/js/temp/script.js
+++ src/js/temp/script.js
@@ -488,6 +488,23 @@ document.addEventListener("componentsLoaded", () => {
   if (currentPath.endsWith("/")) {
     modifiedCurrentPath += "index.html";
   }
+  // パスのパターンと対応する変更後のパスを定義
+  const pathPatterns = [
+    { pattern: /^\/pc\/bycle-best\/(compensation|premium|roadservice)\.html$/, newPath: "/pc/bycle-best/index.html" },
+    { pattern: /^\/pc\/bycle\/(compensation|premium|roadservice)\.html$/, newPath: "/pc/bycle/index.html" },
+    { pattern: /^\/pc\/bycle-s\/(compensation|premium|roadservice)\.html$/, newPath: "/pc/bycle-s/index.html" },
+  ];
+
+  // URLに'bycle'が含まれる場合のみパターンマッチングを実行
+  if (currentPath.includes("bycle")) {
+    pathPatterns.forEach(({ pattern, newPath }) => {
+      if (pattern.test(currentPath)) {
+        modifiedCurrentPath = newPath;
+      }
+    });
+  }
+
+  console.log(modifiedCurrentPath);
   const navItems = document.querySelectorAll(".l-header-nav__item");
   // 各要素をループして処理
   navItems.forEach((item) => {
