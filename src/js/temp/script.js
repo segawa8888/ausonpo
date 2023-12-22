50e0620aad97f59744220a49aaf683af721f06ba
Kosuke Naito(Pocket Inc.) <naito@pocket-kobe.co.jp>
2023-12-22 16:32:56 +0900

    add / ドロワーメニュー、背景クリック時に閉じる処理を追加

--- src/js/temp/script.js
+++ src/js/temp/script.js
@@ -2,27 +2,37 @@
 Pocket.Inc ドロワーメニュー - 2023-05-16
 ----------------------------------------------------------*/
 //コンポーネント読み込み後
-document.addEventListener("componentsLoaded", () => {
+document.addEventListener('componentsLoaded', () => {
   /* 初期設定 */
-  const drawer = document.querySelector(".js-drawer"); //メニュー要素
-  const headerNavBtn = document.querySelector(".js-header-menu"); //メニューボタン
-  const headerNavBtnText = document.querySelector(".js-header-menu-text"); //メニューテキスト
+  const drawer = document.querySelector('.js-drawer'); //メニュー要素
+  const headerNavBtn = document.querySelector('.js-header-menu'); //メニューボタン
+  const headerNavBtnText = document.querySelector('.js-header-menu-text'); //メニューテキスト
 
   if (drawer) {
-    headerNavBtn.addEventListener("click", toggleDrawerMenu);
-    headerNavBtn.addEventListener("keydown", function (e) {
-      if (e.key === "Enter") {
+    headerNavBtn.addEventListener('click', toggleDrawerMenu);
+    drawer.addEventListener('click', toggleDrawerMenu); //背景クリックでメニューを閉じる
+
+    //内部要素をクリック時はメニューを閉じない
+    document.querySelector('.l-drawer__main').addEventListener('click', function (event) {
+      event.stopPropagation();
+    });
+    document.querySelector('.l-drawer__sub').addEventListener('click', function (event) {
+      event.stopPropagation();
+    });
+
+    headerNavBtn.addEventListener('keydown', function (e) {
+      if (e.key === 'Enter') {
         toggleDrawerMenu();
       }
     });
     //ドロワーメニューの最後のリンクでtabを押下時にメニューを閉じる処理
-    const focusableElm = drawer.querySelectorAll("a, button, input, select, textarea");
+    const focusableElm = drawer.querySelectorAll('a, button, input, select, textarea');
     const lastFocusableElm = focusableElm[focusableElm.length - 1];
-    lastFocusableElm.addEventListener("keydown", function (e) {
-      if (e.key === "Tab" && !e.shiftKey) {
+    lastFocusableElm.addEventListener('keydown', function (e) {
+      if (e.key === 'Tab' && !e.shiftKey) {
         // 例えば、フォーカスをドロワーメニューの最初の要素に戻す
         e.preventDefault(); // デフォルトのTabキーの挙動を防止
-        document.querySelector(".l-header__logo a").focus();
+        document.querySelector('.l-header__logo a').focus();
         toggleDrawerMenu();
       }
     });
@@ -30,15 +40,15 @@ document.addEventListener("componentsLoaded", () => {
 
   //ドロワーメニューの表示
   function toggleDrawerMenu() {
-    const isActive = headerNavBtn.classList.toggle("is-active");
+    const isActive = headerNavBtn.classList.toggle('is-active');
 
     setScrollbarWidth(); // スクロールバーの幅を取得
-    body.style.paddingRight = isActive ? scrollBarWidth : "";
+    body.style.paddingRight = isActive ? scrollBarWidth : '';
     // headerNavBtn.style.marginRight = isActive ? scrollBarWidth : "";
 
-    html.classList.toggle("is-drawer-open", isActive);
+    html.classList.toggle('is-drawer-open', isActive);
     bodyScrollPrevent(isActive); // 背景スクロールの制御
-    headerNavBtnText.textContent = isActive ? "閉じる" : "メニュー";
+    headerNavBtnText.textContent = isActive ? '閉じる' : 'メニュー';
 
     // ドロワーメニューの状態に基づいて tabindex を制御
     if (isActive) {
@@ -50,20 +60,20 @@ document.addEventListener("componentsLoaded", () => {
 
   // ドロワーメニュー表示時にtabでのセレクトを無効にする
   function disableTabOutsideDrawer() {
-    const focusableElm = document.querySelectorAll("a, button, input, select, textarea");
+    const focusableElm = document.querySelectorAll('a, button, input, select, textarea');
     focusableElm.forEach((elm) => {
-      if (!elm.closest(".js-drawer")) {
+      if (!elm.closest('.js-drawer')) {
         // ドロワーメニュー内の要素を除外
-        elm.setAttribute("tabindex", "-1");
+        elm.setAttribute('tabindex', '-1');
       }
     });
   }
 
   // ドロワーメニュー非表示時にtabでのセレクトを有効化
   function enableTabOutsideDrawer() {
-    const focusableElm = document.querySelectorAll("a, button, input, select, textarea");
+    const focusableElm = document.querySelectorAll('a, button, input, select, textarea');
     focusableElm.forEach((elm) => {
-      elm.removeAttribute("tabindex");
+      elm.removeAttribute('tabindex');
     });
   }
 });
