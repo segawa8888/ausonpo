c1ab6fb8219d424bace4613bbd6c727c69e9ca2e
Kosuke Naito(Pocket Inc.) <naito@pocket-kobe.co.jp>
2023-12-25 16:03:29 +0900

    update / サブナビゲーション / 採用カテゴリ、下層ページのカレント表示

--- src/js/temp/script.js
+++ src/js/temp/script.js
@@ -492,22 +492,28 @@ document.addEventListener("componentsLoaded", () => {
 Pocket.Inc PCでの電話リンクの無効化処理 - 2023-11-13
 ----------------------------------------------------------*/
 //コンポーネント読み込み後
-document.addEventListener("componentsLoaded", () => {
+document.addEventListener('componentsLoaded', () => {
   // 現在のパスがルート（/）の場合index.htmlを追加
   let modifiedCurrentPath = currentPath;
-  if (currentPath.endsWith("/")) {
-    modifiedCurrentPath += "index.html";
+  if (currentPath.endsWith('/')) {
+    modifiedCurrentPath += 'index.html';
   }
+
+  //採用カテゴリは下層ページも含めて全てカレント表示する
+  if (currentPath.includes('/corporate/recruit/')) {
+    modifiedCurrentPath = '/corporate/recruit/index.html';
+  }
+
   // パスのパターンと対応する変更後のパスを定義
-  const pathPatterns = [
-    { pattern: /^\/pc\/bycle-best\/(compensation|premium|roadservice)\.html$/, newPath: "/pc/bycle-best/index.html" },
-    { pattern: /^\/pc\/bycle\/(compensation|premium|roadservice)\.html$/, newPath: "/pc/bycle/index.html" },
-    { pattern: /^\/pc\/bycle-s\/(compensation|premium|roadservice)\.html$/, newPath: "/pc/bycle-s/index.html" },
+  const pathPatternsBycle = [
+    { pattern: /^\/pc\/bycle-best\/(compensation|premium|roadservice)\.html$/, newPath: '/pc/bycle-best/index.html' },
+    { pattern: /^\/pc\/bycle\/(compensation|premium|roadservice)\.html$/, newPath: '/pc/bycle/index.html' },
+    { pattern: /^\/pc\/bycle-s\/(compensation|premium|roadservice)\.html$/, newPath: '/pc/bycle-s/index.html' },
   ];
 
   // URLに'bycle'が含まれる場合のみパターンマッチングを実行
-  if (currentPath.includes("bycle")) {
-    pathPatterns.forEach(({ pattern, newPath }) => {
+  if (currentPath.includes('bycle')) {
+    pathPatternsBycle.forEach(({ pattern, newPath }) => {
       if (pattern.test(currentPath)) {
         modifiedCurrentPath = newPath;
       }
@@ -515,21 +521,21 @@ document.addEventListener("componentsLoaded", () => {
   }
 
   console.log(modifiedCurrentPath);
-  const navItems = document.querySelectorAll(".l-header-nav__item");
+  const navItems = document.querySelectorAll('.l-header-nav__item');
   // 各要素をループして処理
   navItems.forEach((item) => {
     // 要素内のaタグを探す
-    const link = item.querySelector("a");
+    const link = item.querySelector('a');
     // aタグのhref属性を取得
-    let linkHref = link.getAttribute("href");
+    let linkHref = link.getAttribute('href');
     // href属性の末尾が '/' の場合、'index.html' を追加
-    if (linkHref.endsWith("/")) {
-      linkHref += "index.html";
+    if (linkHref.endsWith('/')) {
+      linkHref += 'index.html';
     }
     // aタグのhref属性が現在のパスと一致するか確認
     if (linkHref === modifiedCurrentPath) {
       // 一致する場合、.-currentクラスを追加
-      item.classList.add("-current");
+      item.classList.add('-current');
     }
   });
 });
